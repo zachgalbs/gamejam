6ecba1f1de7babd59ab82210bcee86e146f88501
zachgalbs <zacharythomasgalbraith@gmail.com>
2023-08-03 20:43:03 -0700

    horizontal movement setup

--- Assets/PlayerBehavior.cs
+++ Assets/PlayerBehavior.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerBehavior : MonoBehaviour
 {
+    // movement horizontal
+    float mH;
+    float speed = 5f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,8 @@ public class PlayerBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        mH = Input.GetAxis("Horizontal");
+
+        transform.position = new Vector3(transform.position.x + mH * speed * Time.deltaTime, transform.position.y);
     }
 }
